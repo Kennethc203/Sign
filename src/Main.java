3157718e1bbcd897614804e95344f123d974bafe
kennethc203 <kennethc203@nycstudents.net>
2025-12-05 15:06:55 -0500

    fixed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("end this existence pls");
         Sign signalling = new Sign("Everything on sale, please come in", 17);
         System.out.println(signalling.getLines());
         System.out.println(signalling.numberOfLines());
